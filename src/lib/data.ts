164c90866494f7642344bbc14c0c237f0384d9a1
Shane Goonasekera <46552619+shanerrr@users.noreply.github.com>
2025-06-28 07:43:22 -0600

    fixed proxy issue

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -10,12 +10,9 @@ export const getFruits = async (): Promise<Fruit[]> => {
   const response = await fetch(`${process.env.BASE_API_URL}/fruits`, {
     headers: {
       "x-api-key": process.env.API_KEY!,
-      origin:
-        process.env.NODE_ENV === "development"
-          ? "http://localhost:3000"
-          : process.env.NEXT_PUBLIC_APP_URL || "https://your-domain.com",
+      origin: "http://localhost:3000",
     },
-    next: { revalidate: 3600 }, // cache for 1 hour
+    next: { revalidate: 1800 }, // cache for 30mins
   });
 
   if (!response.ok) {
